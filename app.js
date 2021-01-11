62e18cf742a22ecd14f8477bedf0d5a8c55a6061
ritikadhiman <ritikadhimanritz@gmail.com>
2021-01-11 17:37:30 +0530

    feat - added output textarea

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
     console.log("Bello!");
-    console.log(txtInput.value);
+    txtOutput.value = txtInput.value;
 });
