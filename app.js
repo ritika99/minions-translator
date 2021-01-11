a029b65fad847d710e61069df31e648224c0f6c6
ritikadhiman <ritikadhimanritz@gmail.com>
2021-01-11 17:23:50 +0530

    feat - added input textarea

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
     console.log("Bello!");
+    console.log(txtInput.value);
 });
