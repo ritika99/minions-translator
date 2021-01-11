08c576f5eefe6c71d34b3d1df9da2ac2fdb8bb86
ritikadhiman <ritikadhimanritz@gmail.com>
2021-01-11 18:23:37 +0530

    feat - Added translation api

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("Bello!");
-    txtOutput.value = txtInput.value;
+const serverUrl = "	https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedUrl(text) {
+    return serverUrl + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error occured: ", error);
+    alert("Try again after sometime!");
+}
+
+btnTranslate.addEventListener("click", () => {
+    var inputText = txtInput.value;
+
+    fetch(getTranslatedUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            txtOutput.innerText = translatedText;
+        })
+        .catch(errorHandler)
 });
